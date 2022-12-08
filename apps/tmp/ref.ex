a9ad25e2a96c8213978a220a5a9de5d054940e10
Kunal Joshi <kunaljoshi99@outlook.com>
2022-12-08 15:23:37 -0500

    Removed old code

--- apps/tmp/ref.ex
+++
@@ -1,7 +0,0 @@
-x = :enacl.sign_keypair()
-sig = :enacl.sign_detached(inspect(message), x.secret)
-:enacl.sign_verify_detached(sig, inspect(message), x.public)
-
-{pub, priv} = :crypto.generate_key(:eddsa, :ed25519)
-signature = :crypto.sign(:eddsa, :none, inspect(message), [priv, :ed25519])
-:crypto.verify(:eddsa, :none, inspect(message), signature, [pub, :ed25519])
